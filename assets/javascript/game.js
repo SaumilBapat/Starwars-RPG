b0758de0859ef234c8c550b9f723cd48b0651022
SaumilBapat <sbapat@salesforce.com>
2018-10-18 21:30:25 -0400

    Dynamic Rendering Characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 
 var characters = [
-  {name: "Darth Maul", picture: "./assets/images/darth-maul.png", health: 120},
-  {},
-  {},
-  {},
+  {name: "Darth Maul", picture: "./assets/images/darth-maul.png", health: 120, status: "start"},
+  {name: "Darth Sidious", picture: "./assets/images/darth-sidious.jpg", health: 120, status: "start"},
+  {name: "Luke Skywalker", picture: "./assets/images/luke-skywalker.jpg", health: 120, status: "start"},
+  {name: "Obi Wan Kenobi", picture: "./assets/images/obi-wan-kenobi.jpg", health: 120, status: "start"},
 ];
 var currentCharacter = 0;
 var word = [];
@@ -12,20 +12,18 @@ var guessesRemaining = 5;
 var wins = 0;
 var losses = 0;
 $(document).ready(function() {
-  window.alert( "ready!" );
   renderYourCharacters();
 });
 
 
 function renderYourCharacters() {
-  var yourCharacters = $("#YourCharacters").empty();
-  yourCharacters.empty();
-  // for (var i = 0; i < characters.length; i++) {
-  //   var character = characters[i];
-  //   var letter = $("<div>");
-  //   letter.addClass("letter");
-  //   letter.attr("data-letter", selectedLetters[i]);
-  //   letter.text(character.name);
-  //   $("#YourCharacters").append(letter);
-  // }
+  $("#YourCharacters").empty();
+  for (var i = 0; i < characters.length; i++) {
+    if(characters[i].status === "start" || characters[i].status === "chosen") {
+      var charDiv = $("<div>");
+      charDiv.addClass("character");
+      charDiv.html("<div class='characterInfo'>" + characters[i].name + "</div><image src='" + characters[i].picture + "' class='characterIcon'/><div class='characterInfo'>" + characters[i].health + "</div>");
+      $("#YourCharacters").append(charDiv);
+    }
+  }
 }
